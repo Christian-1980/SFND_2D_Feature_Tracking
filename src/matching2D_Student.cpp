6b23771fbc9f4420340310221646bb44266c6a7c
Christian--1980 <ch.gpunkt@gmail.com>
2025-03-14 16:46:36 +0100

    added dscriptor and knn matcher

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -18,7 +18,12 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
-        // ...
+        if (descSource.type() != CV_32F || descRef.type() != CV_32F) 
+        {
+            descSource.convertTo(descSource, CV_32F);
+            descRef.convertTo(descRef,CV_32F);
+        }
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
     }
 
     // perform matching task
@@ -28,9 +33,22 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
         matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
     }
     else if (selectorType.compare("SEL_KNN") == 0)
-    { // k nearest neighbors (k=2)
+    { 
+        // k nearest neighbors (k=2)
+        int k = 2;
 
-        // ...
+        // vector to store the 2 neighbours
+        vector<vector<cv::DMatch>> knn_matches;
+        matcher->knnMatch(descSource, descRef, knn_matches, k);
+        
+        // Filter matches using descriptor distance ratio test
+        double minDescDistRatio = 0.8;
+        for (auto it : knn_matches) {
+            // Check if the  2 matches are near to each other % if so psuh to resulting vector
+            if ( 2 == it.size() && (it[0].distance < minDescDistRatio * it[1].distance) ) {
+                matches.push_back(it[0]);
+            }
+        }
     }
 }
 
